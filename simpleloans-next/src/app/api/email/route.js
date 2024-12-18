d535f6214b31fed76d7e9f0da017b2b285a6fbdb
patrickpcodes <patrickpetropoulos@protonmail.com>
2024-12-17 19:15:23 -0500

    add test

--- simpleloans-next/src/app/api/email/route.js
+++ simpleloans-next/src/app/api/email/route.js
@@ -1,6 +1,11 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(req) {
+  if (
+    req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  ) {
+    return res.status(401).end("Unauthorized");
+  }
   try {
     console.log("Cron job triggered at:", new Date().toISOString());
 
