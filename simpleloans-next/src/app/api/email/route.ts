187bdaada13d25bfc58b62f7bb220de207620299
patrickpcodes <patrickpetropoulos@protonmail.com>
2024-12-18 17:02:58 -0500

    trying new cron

--- simpleloans-next/src/app/api/email/route.ts
+++ simpleloans-next/src/app/api/email/route.ts
@@ -10,12 +10,9 @@ export async function GET() {
     console.log("Cron job triggered at:", new Date().toISOString());
 
     // Call another internal API endpoint
-    const response = await fetch(
-      "https://simpleloans.vercel.app/api/email/sendTest",
-      {
-        method: "GET",
-      }
-    );
+    const response = await fetch("/api/email/sendTest", {
+      method: "GET",
+    });
 
     const data = await response.json();
     console.log("Response from other API:", data);
