6aead202443010ad12678b5d120c2903cdb81606
patrickpcodes <patrickpetropoulos@protonmail.com>
2024-12-17 19:13:32 -0500

    update api call

--- simpleloans-next/src/app/api/email/route.js
+++ simpleloans-next/src/app/api/email/route.js
@@ -1,22 +1,30 @@
-export default async function handler(req, res) {
+import { NextResponse } from "next/server";
+
+export async function GET() {
   try {
     console.log("Cron job triggered at:", new Date().toISOString());
 
     // Call another internal API endpoint
-    const response = await fetch(
-      "https://simpleloans.vercel.app/api/email/sendTest",
-      {
-        method: "GET",
-      }
-    );
+    const response = await fetch("/api/email/sendTest", {
+      method: "GET",
+    });
 
     const data = await response.json();
     console.log("Response from other API:", data);
-
+    return NextResponse.json({
+      status: 200,
+      message: "Cron job executed successfully",
+      data,
+    });
     // Perform your task logic here
-    res.status(200).json({ message: "Cron job executed successfully", data });
+    // res.status(200).json({ message: "Cron job executed successfully", data });
   } catch (error) {
-    console.error("Error in cron job:", error);
-    res.status(500).json({ message: "Cron job failed", error: error.message });
+    return NextResponse.json({
+      status: 500,
+      message: "Cron job failed",
+      error: error.message,
+    });
+    // console.error("Error in cron job:", error);
+    // res.status(500).json({ message: "Cron job failed", error: error.message });
   }
 }
