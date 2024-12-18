75430e3d72bf8b93ca3ff9165f35cb8c9f861386
patrickpcodes <patrickpetropoulos@protonmail.com>
2024-12-18 16:46:14 -0500

    test cron

--- simpleloans-next/src/app/api/email/route.ts
+++ simpleloans-next/src/app/api/email/route.ts
@@ -1,18 +1,21 @@
 import { NextResponse } from "next/server";
 
-export async function GET(req) {
-  if (
-    req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
-  ) {
-    return res.status(401).end("Unauthorized");
-  }
+export async function GET(request: Request, res: NextResponse) {
+  // if (
+  //   req.headers.get("Authorization") !== `Bearer ${process.env.CRON_SECRET}`
+  // ) {
+  //   return res.status(401).end("Unauthorized");
+  // }
   try {
     console.log("Cron job triggered at:", new Date().toISOString());
 
     // Call another internal API endpoint
-    const response = await fetch("/api/email/sendTest", {
-      method: "GET",
-    });
+    const response = await fetch(
+      "https://simpleloans.vercel.app/api/email/sendTest",
+      {
+        method: "GET",
+      }
+    );
 
     const data = await response.json();
     console.log("Response from other API:", data);
@@ -27,7 +30,7 @@ export async function GET(req) {
     return NextResponse.json({
       status: 500,
       message: "Cron job failed",
-      error: error.message,
+      // error: error.message,
     });
     // console.error("Error in cron job:", error);
     // res.status(500).json({ message: "Cron job failed", error: error.message });
