90b71041378e5570b5753500b7b4fb4dd225061d
patrickpcodes <patrickpetropoulos@protonmail.com>
2025-02-19 15:49:32 -0500

    update

--- simpleloans-next/src/app/(rs)/info/page.tsx
+++ simpleloans-next/src/app/(rs)/info/page.tsx
@@ -5,11 +5,22 @@ export default function Info() {
   return (
     <div>
       <div>
-        <h2 className="text-2xl">Customer Page</h2>
+        <h2 className="text-2xl">Steps to Create New Customer</h2>
         <h6>Click on Customer Button</h6>
         <Image
           className="m-0 rounded-xl"
           src="/images/customer-icon.png"
+          width={300}
+          height={300}
+          sizes="300px"
+          alt="Page Not Found"
+          priority={true}
+          title="Page Not Found"
+        />
+        <h6>Click on Create New Customer</h6>
+        <Image
+          className="m-0 rounded-xl"
+          src="/images/customer-new.png"
           width={300}
           height={300}
           sizes="300px"
